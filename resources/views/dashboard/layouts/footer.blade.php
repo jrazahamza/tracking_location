84947e115abd7a697d189ad78ea05e74cec8dce5
jrazahamzagit config --global user.email <jrazahamza@gmail.com>
2025-08-26 18:09:02 +0500

    footer changes

--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="app-footer">
     <!--begin::To the end-->
-    <div class="float-end d-none d-sm-inline">Anything you want</div>
+    <div class="float-end d-none d-sm-inline"></div>
     <!--end::To the end-->
     <!--begin::Copyright-->
     <strong>
-         &copy; 2022&nbsp;
+         &copy; 2025&nbsp;
         <a href="https://adminlte.io" class="text-decoration-none">Location Finder </a> Inc..
     </strong>
     All rights reserved.
